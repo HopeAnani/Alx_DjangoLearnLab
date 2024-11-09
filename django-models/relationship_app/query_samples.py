7dc9a3126ca0e06e27fb9647c149cece94773e66
Anani Samuel <ananisamuelhope@gmail.com>
2024-11-09 19:15:12 +0300

    edited

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -11,6 +11,11 @@ def list_books_in_library(library_name):
     return books
 
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    librarian = library.librarian
-    return librarian
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = library.librarian
+        return librarian
+    except Library.DoesNotExist:
+        return f"No library found with the name {library_name}"
+    except Librarian.DoesNotExist:
+        return f"No librarian assigned to the library {library_name}"
