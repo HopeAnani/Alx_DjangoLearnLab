ceb27786d38c730e624e76feb607be590aca2909
Anani Samuel <ananisamuelhope@gmail.com>
2024-11-16 17:17:02 +0300

    upd

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.contrib.auth.decorators import permission_required
-from django.http import HttpResponseForbidden
-from django.shortcuts import render, get_object_or_404
 
 from .models import Book
 
-@permission_required('your_app.can_edit', raise_exception=True)
-def edit_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    # Process form submission for editing the book
-    return render(request, 'edit_book.html', {'book': book})
+
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
