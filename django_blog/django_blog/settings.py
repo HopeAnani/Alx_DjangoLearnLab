1f56a27326d23cf0cfc484a3612b778db8c3379c
Anani Samuel <ananisamuelhope@gmail.com>
2024-12-04 08:47:50 +0300

    fixed some issues

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog',  # Database name you just created
+        'USER': 'postgres',  # The PostgreSQL user you created
+        'PASSWORD': '12345678',  # The password for that user
+        'HOST': 'localhost',  # Default for local development
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
