406dd0f20d4987478ca19aa10b15d2beb7afbba0
Anani Samuel <ananisamuelhope@gmail.com>
2024-12-05 11:25:46 +0300

    edited

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -22,9 +22,7 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content', 'tags']
 
-        widgets = {
-            'tags': TagWidget(attrs={'class': 'form-control', 'placeholder': 'Add tags separated by commas'}),
-        }
+        TagWidget(attrs={'class': 'form-control', 'placeholder': 'Add tags separated by commas'}),
 
 class CommentForm(forms.ModelForm):
     class Meta:
